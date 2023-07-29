d33c9442094abcbeff3d252d129ed6b6bc443bb5
kisah <data.kisahtegar@gmail.com>
2023-07-29 15:46:08 +0700

    Connect to database and CRUD for customers

--- main.go
+++ main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kisahtegar/bootcamp-api-hmsi/connectDB"
+	"github.com/kisahtegar/bootcamp-api-hmsi/query"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	err := godotenv.Load("config/.env")
+
+	if err != nil {
+		log.Error().Msg(err.Error())
+		os.Exit(1)
+	}
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_PORT := os.Getenv("DB_PORT")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_DRIVER := os.Getenv("DB_DRIVER")
+	PORT := os.Getenv("PORT")
+
+	log.Info().Msg(DB_HOST)
+	log.Info().Msg(DB_NAME)
+	log.Info().Msg(DB_PORT)
+	log.Info().Msg(DB_USER)
+	log.Info().Msg(DB_PASSWORD)
+	log.Info().Msg(DB_DRIVER)
+	log.Info().Msg(PORT)
+
+	db, errConn := connectDB.GetConnPostgres(DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_DRIVER)
+	if errConn != nil {
+		log.Error().Msg(errConn.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Successfully connected!")
+
+	// DB struct initialize
+	DB := query.DB{Conn: db}
+
+	// Create customer
+	err = DB.Create(&query.Customers{
+		Name:  "Kisah Tegar",
+		Phone: "0822736133",
+		Email: "kisah@mail.com",
+		Age:   18,
+	})
+	fmt.Println("err", err)
+	if err != nil {
+		log.Error().Msg(errConn.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Insert Data Berhasil")
+
+	// Read customer
+	result, err := DB.Read()
+	if err != nil {
+		log.Error().Msg(errConn.Error())
+		os.Exit(1)
+	}
+	fmt.Println(result)
+
+	// Update customer
+	err = DB.Update(&query.Customers{
+		Id:    1,
+		Name:  "Budi",
+		Phone: "012345564",
+		Email: "budi@example.com",
+		Age:   31,
+	})
+	if err != nil {
+		log.Error().Msg(errConn.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Update Berhasil")
+
+	// Delete customer
+	err = DB.Delete(1)
+	if err != nil {
+		log.Error().Msg(errConn.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Delet Berhasil")
+
 }
